509f13f2c13fa7f66fada332de49af35d3c2e4e7
julio cesar <juliocpsjunior@gmail.com>
2026-05-27 12:53:23 -0300

    feat: Controller do Desafio. :white_check_mark:

---
+++ desafio/src/main/java/br/com/poc/desafio/controller/DesafioController.java
@@ -0,0 +1,34 @@
+package br.com.poc.desafio.controller;
+
+
+import br.com.poc.desafio.service.DesafioService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/file")
+public class DesafioController {
+
+    public final DesafioService service;
+
+    public DesafioController (DesafioService service){
+        this.service = service;
+    }
+
+
+    @PostMapping("/save")
+    public ResponseEntity saveFile(@RequestBody String string, SessionStatus sessionStatus) throws IOException {
+        service.saveString(string);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity getFile()throws IOException{
+        service.getString();
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
